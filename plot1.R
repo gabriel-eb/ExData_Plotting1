4721e86174050ef4c814509c40388e2e358d1a2c
gabriel-eb <gabo_espburg@hotmail.com>
2020-05-02 23:10:36 -0500

    Fixed issues with file pathing.

--- plot1.R
+++ plot1.R
@@ -7,14 +7,14 @@
 
 #Download the file, extract the file from zip and read into R.
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(url,destfile = ".//data.zip",method = "curl")
-fileName <- unzip(".//data.zip", list = TRUE)[1,1]
-unzip(".//data.zip",fileName)
+download.file(url,destfile = "data.zip",method = "curl")
+fileName <- unzip("data.zip", list = TRUE)[1,1]
+unzip("data.zip",fileName)
 
 # My device doesn't have too much memory, so I only read the important dates.
-dNames <- read.table(paste(".//", fileName, sep=""), header = TRUE, sep = ";",
+dNames <- read.table(fileName, header = TRUE, sep = ";",
                      nrows=1)
-data <- read.table(paste(".//", fileName, sep=""), sep = ";", na.strings = '?',
+data <- read.table(fileName, sep = ";", na.strings = '?',
                    col.names = names(dNames),
                    colClasses = c("character", "character", "numeric",
                                   "numeric", "numeric", "numeric",
--- plot3.R
+++ plot3.R
@@ -7,14 +7,14 @@
 
 #Download the file, extract the file from zip and read into R.
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(url,destfile = ".//data.zip",method = "curl")
-fileName <- unzip(".//data.zip", list = TRUE)[1,1]
-unzip(".//data.zip",fileName)
+download.file(url,destfile = "data.zip",method = "curl")
+fileName <- unzip("data.zip", list = TRUE)[1,1]
+unzip("data.zip",fileName)
 
 # My device doesn't have too much memory, so I only read the important dates.
-dNames <- read.table(paste(".//", fileName, sep=""), header = TRUE, sep = ";",
+dNames <- read.table(fileName, header = TRUE, sep = ";",
                      nrows=1)
-data <- read.table(paste(".//", fileName, sep=""), sep = ";", na.strings = '?',
+data <- read.table(fileName, sep = ";", na.strings = '?',
                    col.names = names(dNames),
                    colClasses = c("character", "character", "numeric",
                                   "numeric", "numeric", "numeric",
@@ -34,9 +34,8 @@ data$Time <- with(data,strptime(Time,"%Y-%m-%d %H:%M:%S"))
 ############################
 
 png("plot3.png", width = 480, height = 480)
-plot(data$Time,data$Sub_metering_1,type = "n",
+plot(data$Time,data$Sub_metering_1,type = "l",
      ylab = "Energy sub metering", xlab = "")
-lines(data$Time,data$Sub_metering_1)
 lines(data$Time,data$Sub_metering_2, col = "red")
 lines(data$Time,data$Sub_metering_3, col = "blue")
 legend("topright",names(data)[7:9], lwd = 1, col= c("black","red","blue"))
